f620a815ada1f8edee65610eec688088074bedf7
Carles <carles.manas.nicolas@gmail.com>
2021-09-24 10:37:25 +0200

    First commit - Spring boot base

---
+++ src/main/java/cloud/carles/torrentedplex/TorrentedPlexApplication.java
@@ -0,0 +1,13 @@
+package cloud.carles.torrentedplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorrentedPlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorrentedPlexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/carles/torrentedplex/TorrentedPlexApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.carles.torrentedplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorrentedPlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
